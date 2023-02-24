6926a20a55f8845e32274be8d65a8a0865a55642
Edwin Saul <edwinsaulpm@gmail.com>
2023-02-24 02:07:50 -0500

    preguntas leidas desde archivo

--- obtener_preguntas.py
+++ obtener_preguntas.py
@@ -1,17 +1,36 @@
 from random import sample
+import os
+
+#----------------------------
 
 numero_de_preguntas=3 #numero total de preguntas a tomar
+Archivo_preguntas="PreguntasRpa.txt"
+
+#----------------------------
+
+directorio_actual = os.path.abspath(os.path.dirname(__file__))
+ruta_archivo = os.path.join(directorio_actual, Archivo_preguntas)
+contenido_archivo=""
+
+def corrige_input(entrada):
+    entrada=str(entrada)[:]
+    entrada=entrada.replace(" ","")[:]
+    entrada=entrada.replace("\t","")[:]
+    entrada=entrada.replace("\n","")[:]
+    return(entrada[:])
+
+with open(ruta_archivo, mode="r",encoding="utf-8") as archivo: #abre archivo en string
+    contenido_archivo = archivo.read()
+
+lineas=contenido_archivo.split("\n")[:]
+
+print(lineas)
+input()
+
 
 def Todas_las_preguntas():
-    lista=[
-            ["Cuando es 2 y 2?","4"],
-            ["de que color es la bandera del Perú","roja y blanca"],
-            ["hay una palabra clave","churichurinfunflays"],
-            ["si un maya se desmaya es maya","no se"],
-            ["solo se","que nada se"],
-
-            ]
-    return lista
+   lista=[1,2,3,4,45]
+   return lista
 
 def preguntas():
     # devuelve una lista con las preguntas sacadas del temario
@@ -21,6 +40,7 @@ def preguntas():
 
 
 if __name__=="__main__" :
+    print(contenido_archivo)
     print(preguntas())
     input()
 
